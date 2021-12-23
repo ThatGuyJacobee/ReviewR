bc369be97f1b50f57153e5d8cf95d817114f39d7
Jacob <42719082+ThatGuyJacobee@users.noreply.github.com>
2021-12-23 00:52:00 +0000

    v1.36 - 23/12/21
    + Working Google OAuth 2.0 as per their documented example.
    Source: https://github.com/googlesamples/oauth-apps-for-windows
    + Added reviewr.oauth2 protocol which allows for the URI callback.
    + Google API key is set in the Google Developer API console.
    + The Google API runs on button click event.

--- App.xaml.cs
+++ App.xaml.cs
@@ -149,6 +149,34 @@ namespace ReviewR
         }
 
         /// <summary>
+        /// Invoked when the application is launched through a custom URI scheme, such as
+        /// is the case in an OAuth 2.0 authorization flow.
+        /// </summary>
+        /// <param name="args">Details about the URI that activated the app.</param>
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            // When the app was activated by a Protocol (custom URI scheme), forwards
+            // the URI to the MainPage through a Navigate event.
+            if (args.Kind == ActivationKind.Protocol)
+            {
+                // Extracts the authorization response URI from the arguments.
+                ProtocolActivatedEventArgs protocolArgs = (ProtocolActivatedEventArgs)args;
+                Uri uri = protocolArgs.Uri;
+                Debug.WriteLine("Authorization Response: " + uri.AbsoluteUri);
+
+                // Gets the current frame, making one if needed.
+                var frame = Window.Current.Content as Frame;
+                if (frame == null)
+                    frame = new Frame();
+
+                // Opens the URI for "navigation" (handling) on the MainPage.
+                frame.Navigate(typeof(LoginPage), uri); //Go to loginpage to perform oauth database checks
+                Window.Current.Content = frame;
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
         /// Invoked when Navigation to a certain page fails
         /// </summary>
         /// <param name="sender">The Frame which failed navigation</param>
--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -14,6 +14,13 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using MySql.Data.MySqlClient;
 using System.Diagnostics; //Debug
+using Windows.Storage; //Storage and below required for Google OAuth 2.0 Login
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using System.Net.Http;
+using System.Text;
+using Windows.Data.Json;
+using Windows.Storage.Streams;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -154,6 +161,195 @@ namespace ReviewR
         private void database_status_SelectionChanged(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        const string clientID = "139950512543-hh4oeaddj7borputdqi9jt3ju3o9322s.apps.googleusercontent.com";
+        const string redirectURI = "reviewr.oauth2:/oauth2redirect";
+        const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+        const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+        const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+        private void google_signin_Click(object sender, RoutedEventArgs e)
+        {
+            // Generates state and PKCE values.
+            string state = randomDataBase64url(32);
+            string code_verifier = randomDataBase64url(32);
+            string code_challenge = base64urlencodeNoPadding(sha256(code_verifier));
+            const string code_challenge_method = "S256";
+
+            // Stores the state and code_verifier values into local settings.
+            // Member variables of this class may not be present when the app is resumed with the
+            // authorization response, so LocalSettings can be used to persist any needed values.
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values["state"] = state;
+            localSettings.Values["code_verifier"] = code_verifier;
+
+            // Creates the OAuth 2.0 authorization request.
+            string authorizationRequest = string.Format("{0}?response_type=code&scope=openid%20profile&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method={5}",
+                authorizationEndpoint,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                state,
+                code_challenge,
+                code_challenge_method);
+
+            output("Opening authorization request URI: " + authorizationRequest);
+
+            // Opens the Authorization URI in the browser.
+            var success = Windows.System.Launcher.LaunchUriAsync(new Uri(authorizationRequest));
+        }
+
+        /// <summary>
+        /// Processes the OAuth 2.0 Authorization Response
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is Uri)
+            {
+                // Gets URI from navigation parameters.
+                Uri authorizationResponse = (Uri)e.Parameter;
+                string queryString = authorizationResponse.Query;
+                output("MainPage received authorizationResponse: " + authorizationResponse);
+
+                // Parses URI params into a dictionary
+                // ref: http://stackoverflow.com/a/11957114/72176
+                Dictionary<string, string> queryStringParams =
+                        queryString.Substring(1).Split('&')
+                             .ToDictionary(c => c.Split('=')[0],
+                                           c => Uri.UnescapeDataString(c.Split('=')[1]));
+
+                if (queryStringParams.ContainsKey("error"))
+                {
+                    output(String.Format("OAuth authorization error: {0}.", queryStringParams["error"]));
+                    return;
+                }
+
+                if (!queryStringParams.ContainsKey("code")
+                    || !queryStringParams.ContainsKey("state"))
+                {
+                    output("Malformed authorization response. " + queryString);
+                    return;
+                }
+
+                // Gets the Authorization code & state
+                string code = queryStringParams["code"];
+                string incoming_state = queryStringParams["state"];
+
+                // Retrieves the expected 'state' value from local settings (saved when the request was made).
+                ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+                string expected_state = (String)localSettings.Values["state"];
+
+                // Compares the receieved state to the expected value, to ensure that
+                // this app made the request which resulted in authorization
+                if (incoming_state != expected_state)
+                {
+                    output(String.Format("Received request with invalid state ({0})", incoming_state));
+                    return;
+                }
+
+                // Resets expected state value to avoid a replay attack.
+                localSettings.Values["state"] = null;
+
+                // Authorization Code is now ready to use!
+                output(Environment.NewLine + "Authorization code: " + code);
+
+                string code_verifier = (String)localSettings.Values["code_verifier"];
+                performCodeExchangeAsync(code, code_verifier);
+            }
+            else
+            {
+                Debug.WriteLine(e.Parameter);
+            }
+        }
+
+        async void performCodeExchangeAsync(string code, string code_verifier)
+        {
+            // Builds the Token request
+            string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&code_verifier={3}&scope=&grant_type=authorization_code",
+                code,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                code_verifier
+                );
+            StringContent content = new StringContent(tokenRequestBody, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            // Performs the authorization code exchange.
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.AllowAutoRedirect = true;
+            HttpClient client = new HttpClient(handler);
+
+            output(Environment.NewLine + "Exchanging code for tokens...");
+            HttpResponseMessage response = await client.PostAsync(tokenEndpoint, content);
+            string responseString = await response.Content.ReadAsStringAsync();
+            output(responseString);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                output("Authorization code exchange failed.");
+                return;
+            }
+
+            // Sets the Authentication header of our HTTP client using the acquired access token.
+            JsonObject tokens = JsonObject.Parse(responseString);
+            string accessToken = tokens.GetNamedString("access_token");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+            // Makes a call to the Userinfo endpoint, and prints the results.
+            output("Making API Call to Userinfo...");
+            HttpResponseMessage userinfoResponse = client.GetAsync(userInfoEndpoint).Result;
+            string userinfoResponseContent = await userinfoResponse.Content.ReadAsStringAsync();
+            output(userinfoResponseContent);
+        }
+
+        /// <summary>
+        /// Appends the given string to the on-screen log, and the debug console.
+        /// </summary>
+        /// <param name="output">string to be appended</param>
+        public void output(string output)
+        {
+            Debug.WriteLine(output);
+        }
+
+        /// <summary>
+        /// Returns URI-safe data with a given input length.
+        /// </summary>
+        /// <param name="length">Input length (nb. output will be longer)</param>
+        /// <returns></returns>
+        public static string randomDataBase64url(uint length)
+        {
+            IBuffer buffer = CryptographicBuffer.GenerateRandom(length);
+            return base64urlencodeNoPadding(buffer);
+        }
+
+        /// <summary>
+        /// Returns the SHA256 hash of the input string.
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public static IBuffer sha256(string inputString)
+        {
+            HashAlgorithmProvider sha = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha256);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(inputString, BinaryStringEncoding.Utf8);
+            return sha.HashData(buff);
+        }
+
+        /// <summary>
+        /// Base64url no-padding encodes the given input buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string base64urlencodeNoPadding(IBuffer buffer)
+        {
+            string base64 = CryptographicBuffer.EncodeToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
         }
     }
 }

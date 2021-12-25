636da292b0ff1bdbd540415bab2df9a7876521dc
Jacob <42719082+ThatGuyJacobee@users.noreply.github.com>
2021-12-25 18:25:23 +0000

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -29,7 +29,7 @@ namespace ReviewR
     sealed partial class App : Application
     {
         //Create a global method which can be inherited by any class to create a connection to the database
-        private static string connectionString = "server=81.129.255.171;database=reviewr;uid=root;pwd=Martyniak_13;SSL-mode=none;";
+        private static string connectionString = "server=86.163.198.87;database=reviewr;uid=root;pwd=Martyniak_13;SSL-mode=none;";
 
         public static string ConnectionString { get => connectionString; set => connectionString = value; }
 
